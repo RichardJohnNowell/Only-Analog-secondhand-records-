29f59c8c8318e7bda8530fe5ea2f457711b54c41
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-07-08 12:49:55 +0000

    added routing for index.html

--- run.py
+++ run.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 
 #routing here
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
