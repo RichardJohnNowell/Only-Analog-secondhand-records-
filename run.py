e80399bbabc1af5830f7cd7e30939d8996c41c3c
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-07-08 12:47:31 +0000

    importing Flask and setting environment

---
+++ run.py
@@ -0,0 +1,14 @@
+#python core file
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+#routing here
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
