3f967d23552feb96d3a44c5c26b38dd1f061f49f
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-07-08 12:51:36 +0000

    added routing for listings.html

--- run.py
+++ run.py
@@ -10,7 +10,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
+
+@app.route("/listings")
+def listings():
+    return render_template("listings.html")
+
 
 if __name__ == "__main__":
     app.run(
