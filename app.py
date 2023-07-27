05d95f01c3ff17d826acad0e5f05f25cbb7f4e46
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-07-27 07:49:16 +0000

    started app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/listings")
+def listings():
+    return render_template("listings.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/opinions")
+def opinions():
+    return render_template("opinions.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
