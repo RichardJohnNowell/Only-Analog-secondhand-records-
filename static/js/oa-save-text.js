3dd52bba342fbacfc2eec617b28a94a0adc23f88
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-09-04 15:23:29 +0000

    added comments

--- static/js/oa-save-text.js
+++ static/js/oa-save-text.js
@@ -1,23 +1,23 @@
+// to download a text
+// start
 function elemTarget(e) {
 	e = e || window.event;
 	e.preventDefault();
 	return e.target;
 }
-
   document.addEventListener("click", function(e) {
 	let elem = elemTarget(e);
-    
-	if (elem.id == 'buttonDownload') {
+  if (elem.id == 'buttonDownload') {
     saveText();
 	}
 });
-
+// saving text
 function saveText() {
 	let data = document.querySelector('#texTareaI').value;
 	let file = 'data-' + Date.now() + '.txt';
-
-	let link = document.createElement('a');
+  let link = document.createElement('a');
 	link.download = file;
+  // raw data converted to text
 	let blob = new Blob(['' + data + ''], {
 		type: 'text/plain'
 	});
@@ -25,3 +25,4 @@ function saveText() {
 	link.click();
 	URL.revokeObjectURL(link.href);
 }
+// end
