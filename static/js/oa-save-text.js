c08a457b9cdaadbdf9f6b7895baa4c3238b809d7
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-09-04 12:43:48 +0000

    tidied js as in JSHint

--- static/js/oa-save-text.js
+++ static/js/oa-save-text.js
@@ -1,48 +1,40 @@
-// -------------------------- LEVEL I ---------------
-
+// download information in different formats
+// start
+// just download format
 lIDownload.onclick = (function(){
   doDL(document.getElementById("myTextArea").value);
   });
-  
   function doDL(s){
       function dataUrl(data) {return "data:x-application/text," + escape(data);}
       window.open(dataUrl(s));
   }
-  
-  // -------------------------- LEVEL II ---------------
-  
-  const stack = {
+// json format
+   let stack = {
    some: "stuffs",
    alot: "of them!"
-  }
-  
+  };
   jsonDownload.onclick = (function(){
     let j = document.createElement("a");
     j.download = "stack_" + Date.now() + ".json";
     j.href = URL.createObjectURL(new Blob([JSON.stringify(stack, null, 2)]));
-    j.click()
+    j.click();
   });
-  
-  // -------------------------- LEVEL III ---------------
-  
+  // text file
   function elemTarget(e) {
     e = e || window.event;
     e.preventDefault();
     return e.target;
   }
-  
     document.addEventListener("click", function(e) {
     let elem = elemTarget(e);
-      
-    if (elem.id == 'buttonDownload') {
+    if (elem.id === 'buttonDownload') {
       saveText();
     }
   });
-  
+  // save file
   function saveText() {
     let data = document.querySelector('#texTareaI').value;
     let file = 'data-' + Date.now() + '.txt';
-  
     let link = document.createElement('a');
     link.download = file;
     let blob = new Blob(['' + data + ''], {
@@ -52,4 +44,4 @@ lIDownload.onclick = (function(){
     link.click();
     URL.revokeObjectURL(link.href);
   }
-  
+// end
