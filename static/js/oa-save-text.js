32f4f33651a0cd0865a41060d18be7f1a646b10e
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-09-04 09:26:20 +0000

    started js text download file

---
+++ static/js/oa-save-text.js
@@ -0,0 +1,33 @@
+// oa-save-text-as-download
+// start
+function downloadFile(oa_filename, content) {
+    let element = document.createElement(a);
+    // blob is a data type that can store binary data
+    let blob = new Blob([content], { type: plain/text });
+    // creates a DOM string containing a URL which
+    // represents the object given in the parameter
+    let fileUrl = URL.createObjectURL(blob);
+    // sets the value of an attribute on the specified element
+    element.setAttribute(href, fileUrl);
+    element.setAttribute(download, oa_filename);
+    element.style.display = none;
+    // method to move an element from one to another
+    document.body.appendChild(element);
+    element.click();
+    // method removes a child from the DOM
+    document.body.removeChild(element);
+  }
+  window.onload = () => {
+    document.getElementById(download).
+    addEventListener(click, e => {
+      // value of the input box. have to use 'var'
+      var filename = document.getElementById(oa_filename).value;
+      // value of the input in the text area
+      let content = document.getElementById(oa_text).value;
+      // The && operator indicates whether both operands are true.
+      if (oa_filename && content) {
+        downloadFile(oa_filename, content);
+      }
+    }
+  );};
+  // end
