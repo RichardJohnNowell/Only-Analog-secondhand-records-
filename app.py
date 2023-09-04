330f9c49d6909cd36b5e2bd53593b08907ee88ce
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-09-04 15:56:20 +0000

    changed false to true

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=False)
+        debug=True)
 
 
 # end of only analog python file app.py
