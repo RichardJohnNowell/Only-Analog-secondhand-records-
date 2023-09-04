5a7e2cbad06e2aa86e6cf6e6d6ff0eb25862b3c4
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-09-04 14:52:51 +0000

    deleted unwanted code

--- static/js/oa-save-text.js
+++ static/js/oa-save-text.js
@@ -1,47 +1,27 @@
-// download information in different formats
-// start
-// just download format
-lIDownload.onclick = (function(){
-  doDL(document.getElementById("myTextArea").value);
-  });
-  function doDL(s){
-      function dataUrl(data) {return "data:x-application/text," + escape(data);}
-      window.open(dataUrl(s));
-  }
-// json format
-   let stack = {
-   some: "stuffs",
-   alot: "of them!"
-  };
-  jsonDownload.onclick = (function(){
-    let j = document.createElement("a");
-    j.download = "stack_" + Date.now() + ".json";
-    j.href = URL.createObjectURL(new Blob([JSON.stringify(stack, null, 2)]));
-    j.click();
-  });
-  // text file
-  function elemTarget(e) {
-    e = e || window.event;
-    e.preventDefault();
-    return e.target;
-  }
-    document.addEventListener("click", function(e) {
-    let elem = elemTarget(e);
-    if (elem.id === 'buttonDownload') {
-      saveText();
-    }
-  });
-  // save file
-  function saveText() {
-    let data = document.querySelector('#texTareaI').value;
-    let file = 'data-' + Date.now() + '.txt';
-    let link = document.createElement('a');
-    link.download = file;
-    let blob = new Blob(['' + data + ''], {
-      type: 'text/plain'
-    });
-    link.href = URL.createObjectURL(blob);
-    link.click();
-    URL.revokeObjectURL(link.href);
-  }
-// end
+function elemTarget(e) {
+	e = e || window.event;
+	e.preventDefault();
+	return e.target;
+}
+
+  document.addEventListener("click", function(e) {
+	let elem = elemTarget(e);
+    
+	if (elem.id == 'buttonDownload') {
+    saveText();
+	}
+});
+
+function saveText() {
+	let data = document.querySelector('#texTareaI').value;
+	let file = 'data-' + Date.now() + '.txt';
+
+	let link = document.createElement('a');
+	link.download = file;
+	let blob = new Blob(['' + data + ''], {
+		type: 'text/plain'
+	});
+	link.href = URL.createObjectURL(blob);
+	link.click();
+	URL.revokeObjectURL(link.href);
+}
